db500cccdf23f3d7203049655f651cfcf453b21e
James Harrison <james@fasttortoise.co.uk>
2019-09-17 21:45:39 +0100

    Added comments

--- src/main/java/net/capecraft/events/JoinLeave.java
+++ src/main/java/net/capecraft/events/JoinLeave.java
@@ -20,8 +20,9 @@ public class JoinLeave implements Listener {
 	
 	@EventHandler
 	public void onPingEvent(ProxyPingEvent event) {
+		//Shows a custom ping if player isn't on 1.14.4
 		ServerPing response = event.getResponse();
-		response.getVersion().setName("CapeCraft 1.14.4");
+		response.getVersion().setName("Requires MC 1.14.4");
 		response.getVersion().setProtocol(ProtocolConstants.MINECRAFT_1_14_4);
 		event.setResponse(response);
 		
@@ -30,22 +31,32 @@ public class JoinLeave implements Listener {
 	@EventHandler
 	public void onJoinEvent(PostLoginEvent event) {
 		
+		//Kicks player if version not 1.14.4
 		if(event.getPlayer().getPendingConnection().getVersion() != ProtocolConstants.MINECRAFT_1_14_4) {
 			event.getPlayer().disconnect(new ComponentBuilder(Main.PREFIX).append("Please join using version 1.14.4").reset().create());
 		}
 		
+		//Gets msg from config and sends join/leave message		
 		String msgRaw = ConfigurationManager.getPluginConfig().getString("joinMessage");
-		broadcastMessage(msgRaw, event.getPlayer());
+		broadcastJoinLeaveMessage(msgRaw, event.getPlayer());
 	}
 	
 	@EventHandler
 	public void onLeaveEvent(PlayerDisconnectEvent event) {
+		//Gets msg from config and sends join/leave message
 		String msgRaw = ConfigurationManager.getPluginConfig().getString("leaveMessage");
-		broadcastMessage(msgRaw, event.getPlayer());
+		broadcastJoinLeaveMessage(msgRaw, event.getPlayer());
 	}
 	
-	private void broadcastMessage(String msgRaw, ProxiedPlayer player) {
+	/**
+	 * Broadcast join/leave to player
+	 * @param msgRaw The join/leave message
+	 * @param player The ProxiedPlayer object
+	 */
+	private void broadcastJoinLeaveMessage(String msgRaw, ProxiedPlayer player) {
+		//Checks player is on 1.14.4
 		if(player.getPendingConnection().getVersion() == ProtocolConstants.MINECRAFT_1_14_4) {
+			//Formats the message and sends it
 			msgRaw = msgRaw.replace("%player%", player.getDisplayName());
 			BaseComponent[] msg = TextComponent.fromLegacyText(msgRaw, ChatColor.WHITE);
 			ProxyServer.getInstance().broadcast(msg);
