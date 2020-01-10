96f1431c969406a36ffdc2495bbb38b4bdf8942c
James Harrison <james@fasttortoise.co.uk>
2020-01-10 19:20:10 +0000

    Work on /playtimetop

--- src/main/java/net/capecraft/bungee/commands/PlayTimeTopCommand.java
+++ src/main/java/net/capecraft/bungee/commands/PlayTimeTopCommand.java
@@ -2,6 +2,7 @@ package net.capecraft.bungee.commands;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.Collections;
 import java.util.TreeMap;
 
@@ -48,11 +49,19 @@ public class PlayTimeTopCommand extends Command {
 		if (cacheAge > playtimeLastChecked) {
 			ProxyServer.getInstance().getScheduler().runAsync(BungeeMain.INSTANCE, new GeneratePlaytime(sender));
 		} else {
+			//First message
 			sender.sendMessage(new ComponentBuilder("⎯⎯⎯⎯⎯").color(ChatColor.YELLOW).append(" Top 10 users with playtime ").reset().append("⎯⎯⎯⎯⎯").color(ChatColor.YELLOW).create());
-			sender.sendMessage(new ComponentBuilder("§e-----§r Top 10 users with playtime §e-----").create());
-			for (int i = 0; i < 10; i++) {
+			
+			//Loop through TreeSet 10 times and output values
+			playtimeCache.entrySet().stream().limit(10).forEach(value -> {
+				double playtime = value.getKey();
+				playtime = playtime / 60;
+				DecimalFormat df = new DecimalFormat("#.##");
 				
-			}
+				sender.sendMessage(new ComponentBuilder(" " + df.format(playtime) + "hrs " + value.getValue()).create());
+			});
+			
+			//Final message
 			sender.sendMessage(new ComponentBuilder("⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯").color(ChatColor.YELLOW).create());
 		}
 	}
