f2fc0066085ad21f6e3906813432cc52cbd61b96
James Harrison <james@fasttortoise.co.uk>
2020-01-12 17:19:26 +0000

    Recode serverqueuehelper

--- src/main/java/net/capecraft/bungee/helpers/ServerQueueHelper.java
+++ src/main/java/net/capecraft/bungee/helpers/ServerQueueHelper.java
@@ -23,144 +23,120 @@ public class ServerQueueHelper {
 	private static Queue<UUID> survivalQueue = new LinkedList<UUID>();
 	private static Queue<UUID> creativeQueue = new LinkedList<UUID>();
 	
-	//Survival/Creative Queue Loop
-	private static int survivalIndex = 0;
-	private static int creativeIndex = 0;
-	
 	private static int playersOnline = 0;
 	private static int playersMax = 0;
 	
-
 	/**
-	 * Polls the server, checks the payer count and connects them to the server
-	 * @param serverName Server name to poll
+	 * Attempts to connect the player to the server otherwise queues them
+	 * @param player the player
+	 * @param serverName the server name
 	 */
-	public static void pollServer(String serverName) {
-		//Check server has queued players
-		if(getQueueSize(serverName) <= 0) {
-			return;
-		}
-
-		//Starts a server ping to serverName
-		ProxyServer.getInstance().getServerInfo(serverName).ping(new Callback<ServerPing>() {
-			@Override
-			public void done(ServerPing result, Throwable error) {
-				//Makes sure there isn't an error
-				if(error != null) {
-					return;
-				}
-
-				//Sets dynamic variables
-				playersOnline = result.getPlayers().getOnline();
-				playersMax = result.getPlayers().getMax();
-
-				while(getQueueSize(serverName) > 0) {
-
-					//Gets the next queued player
-					ProxiedPlayer queuedPlayer = getNextPlayer(serverName);			
+	public static void connectPlayer(ProxiedPlayer player, String serverName) {
+		ServerInfo targetServer = ProxyServer.getInstance().getServerInfo(serverName);
+		
+		if(getQueueSize(serverName) == 0) {
+			targetServer.ping(new Callback<ServerPing>() {
+				
+				@Override
+				public void done(ServerPing result, Throwable error) {
+					playersOnline = result.getPlayers().getOnline();
+					playersMax = result.getPlayers().getMax();
 					
-					//Check if server is full
-					if(playersOnline >= playersMax) {
-						//If player is alt kick them
-						if(queuedPlayer.hasPermission(Main.Groups.ALT)) {
-							//Send disconnection messages
-							BaseComponent[] disconnectMsg = TextComponent.fromLegacyText(PluginConfig.getPluginConfig().getString(PluginConfig.FULL_AFK));
-							queuedPlayer.sendMessage(new ComponentBuilder(Main.PREFIX).append(disconnectMsg).reset().create());
-
-							//Remove player from queue
-							removePlayer(queuedPlayer.getUniqueId());
+					//If there is space, move player to server
+					//Else if player is an ALT don't let them in
+					//Else check for AFK players to make space
+					if(playersOnline < playersMax) {
+						player.connect(targetServer);
+						return;
+					} else if(player.hasPermission(Main.Groups.ALT)) {
+						BaseComponent[] disconnectMsg = TextComponent.fromLegacyText(PluginConfig.getPluginConfig().getString(PluginConfig.FULL_AFK));
+						player.sendMessage(new ComponentBuilder(Main.PREFIX).append(disconnectMsg).reset().create());
+						return;
+					} else {
+						//Create disconnects message
+						BaseComponent[] disconnectMsg = TextComponent.fromLegacyText(PluginConfig.getPluginConfig().getString(PluginConfig.KICK_AFK));
+						BaseComponent[] disconnectBroadcast = TextComponent.fromLegacyText(PluginConfig.getPluginConfig().getString(PluginConfig.KICK_AFK_BROADCAST));
+						
+						//Try and kick AFK players until players are less than max
+						ProxiedPlayer afkPlayer = AfkHelper.getNextPlayer(serverName);
+						while(afkPlayer != null) {
+							afkPlayer.disconnect(new ComponentBuilder(Main.PREFIX).append(disconnectMsg).reset().create());
+							afkPlayer = AfkHelper.getNextPlayer(serverName);
+							playersOnline--;
+							
+							if(playersOnline < playersMax) break;							
+						}
+						
+						//If players are less than max try connect our player
+						//Else add player to the queue
+						if(playersOnline < playersMax) {
+							ProxyServer.getInstance().getScheduler().schedule(BungeeMain.INSTANCE, new Runnable() {
+								@Override
+								public void run() {
+									player.connect(targetServer);
+								}
+							}, 1, TimeUnit.SECONDS);
+							
+							ProxyServer.getInstance().getServerInfo(serverName).getPlayers().forEach(player -> {
+								player.sendMessage(new ComponentBuilder(Main.PREFIX).append(disconnectBroadcast).reset().create());
+							});
+							return;
+						} else {
+							addPlayerToQueue(player, serverName);							
+							sendQueueMessage(player.getUniqueId(), 1);
 							return;
 						}
-
-						//Make sure there are players in AFK the server server
-						if(AfkHelper.getQueueList(serverName).size() > 0) {
-							//Get messages
-							BaseComponent[] disconnectMsg = TextComponent.fromLegacyText(PluginConfig.getPluginConfig().getString(PluginConfig.KICK_AFK));
-							BaseComponent[] disconnectBroadcast = TextComponent.fromLegacyText(PluginConfig.getPluginConfig().getString(PluginConfig.KICK_AFK_BROADCAST));
-																					
-							ProxiedPlayer afkPlayer = AfkHelper.getNextPlayer(serverName);
-							while(afkPlayer != null) {
-								afkPlayer.disconnect(new ComponentBuilder(Main.PREFIX).append(disconnectMsg).reset().create());
-								afkPlayer = AfkHelper.getNextPlayer(serverName);
-								playersOnline--;
-							}
-							
-							//Broadcast to players in that server
-							for(ProxiedPlayer players : ProxyServer.getInstance().getServerInfo(serverName).getPlayers()) {
-								players.sendMessage(new ComponentBuilder(Main.PREFIX).append(disconnectBroadcast).reset().create());
-						    }
-
-							//Send queue message
-							sendQueueMessages(serverName);							
-						} else {
-							//Generate queue message
-							String queuePos = String.valueOf(ServerQueueHelper.getQueueSize(serverName));
-							String msgRaw = PluginConfig.getPluginConfig().getString(PluginConfig.QUEUE_MESSAGE);
-							msgRaw = msgRaw.replace("%place%", queuePos);
-							BaseComponent[] msg = new ComponentBuilder(Main.PREFIX).append(TextComponent.fromLegacyText(msgRaw, ChatColor.WHITE)).reset().create();
-							queuedPlayer.sendMessage(msg);
-						}
-					}
-
-					//Send player to server if the playersonline is now less than the max
-					if(playersOnline < playersMax) {
-						ProxyServer.getInstance().getScheduler().schedule(BungeeMain.INSTANCE, new Runnable() {
-							@Override
-							public void run() {
-								ServerInfo serverInfo = ProxyServer.getInstance().getServerInfo(serverName);
-								queuedPlayer.connect(serverInfo, new Callback<Boolean>() {
-									@Override
-									public void done(Boolean result, Throwable error) {									
-										if(result) {
-											//Check if player is an alt and add to AFK queue
-											if(queuedPlayer.hasPermission(Main.Groups.ALT)) {
-												AfkHelper.addAltPlayer(queuedPlayer, serverInfo);
-											}
-											
-											//Removes that player
-											removePlayer(queuedPlayer.getUniqueId());
-	
-											//Increase online variable
-											playersOnline++;
-	
-											//Send queue message
-											sendQueueMessages(serverName);
-										} else {
-											queuedPlayer.sendMessage(new ComponentBuilder(Main.PREFIX).append("An error has occured").reset().create());
-										}
-									}
-								});
-							}
-							
-						}, 1, TimeUnit.SECONDS);
 					}
 				}
-			}
-		});
+			});
+		}
+	}
+	
+	/**
+	 * Polls the server and attempts to join users from a queue
+	 * @param serverName The Server to test
+	 */
+	public static void pollServer(String serverName) {
+		//The server to connect to
+		ServerInfo targetServer = ProxyServer.getInstance().getServerInfo(serverName);
+		
+		//Make sure queue isn't empty
+		if(getQueueSize(serverName) > 0) {
+			
+			//Pins the server and checks online < max. If so tries to join players
+			targetServer.ping(new Callback<ServerPing>() {
+				@Override
+				public void done(ServerPing result, Throwable error) {
+					if(result.getPlayers().getOnline() < result.getPlayers().getMax()) {
+						getNextPlayer(serverName).connect(targetServer);
+					}
+				}
+			});
+		}
 	}
 
 	/**
 	 * Gets all players in queue to send messages to
 	 * @param serverName Server Name to send queue message for
 	 */
-	private static void sendQueueMessages(String serverName) {
-		//The starting queue pos
-		int queuePlace = 1;
-
-		if(serverName.equals(Main.Servers.SURVIVAL)) {
-			//Loops through survivalQueue to send messages
-			for(UUID uuid : survivalQueue) {
-				sendQueueMessage(uuid, queuePlace);
-				queuePlace++;
-			}
-		} else if(serverName.equals(Main.Servers.CREATIVE)) {
-			//Loops through creative queue to send messages
-			for(UUID uuid : creativeQueue) {
-				sendQueueMessage(uuid, queuePlace);
-				queuePlace++;
-			}
-		}
-	}
+//	private static void sendQueueMessages(String serverName) {
+//		//The starting queue pos
+//		int queuePlace = 1;
+//
+//		if(serverName.equals(Main.Servers.SURVIVAL)) {
+//			//Loops through survivalQueue to send messages
+//			for(UUID uuid : survivalQueue) {
+//				sendQueueMessage(uuid, queuePlace);
+//				queuePlace++;
+//			}
+//		} else if(serverName.equals(Main.Servers.CREATIVE)) {
+//			//Loops through creative queue to send messages
+//			for(UUID uuid : creativeQueue) {
+//				sendQueueMessage(uuid, queuePlace);
+//				queuePlace++;
+//			}
+//		}
+//	}
 
 	/**
 	 * Sends the final queue message
@@ -229,35 +205,28 @@ public class ServerQueueHelper {
 			return;
 		}
 
+		connectPlayer(player, serverName);
+	}
+	
+	/**
+	 * Removes the player from any queues
+	 * @param uuid Player uuid
+	 */
+	public static void removePlayer(UUID uuid) {
+		survivalQueue.remove(uuid);
+		creativeQueue.remove(uuid);
+	}
+	
+	/**
+	 * Insert the player into the correct queue
+	 * @param player Player to insert
+	 * @param serverName Server name
+	 */
+	private static void addPlayerToQueue(ProxiedPlayer player, String serverName) {
 		if(serverName.equals(Main.Servers.SURVIVAL)) {
 			survivalQueue.add(player.getUniqueId());
 		} else if(serverName.equals(Main.Servers.CREATIVE)) {
 			creativeQueue.add(player.getUniqueId());
-		}
-
-		pollServer(serverName);
-	}
-
-	/**
-	 * Removes a player from the queue and updates other players
-	 * @param uuid The players uuid
-	 */
-	public static void removePlayer(UUID uuid) {
-
-		if(survivalQueue.contains(uuid)) {
-			survivalQueue.remove(uuid);
-			survivalQueue.forEach(sUUID -> {
-				sendQueueMessage(sUUID, survivalIndex+1);
-				survivalIndex++;
-			});
-		}
-		
-		if(creativeQueue.contains(uuid)) {
-			creativeQueue.remove(uuid);		
-			creativeQueue.forEach(cUUID -> {
-				sendQueueMessage(cUUID, creativeIndex+1);
-				creativeIndex++;
-			});
 		}
 	}
 }
