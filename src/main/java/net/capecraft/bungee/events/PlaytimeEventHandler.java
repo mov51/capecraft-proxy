04322476d6a1f52a32279cd3c21d46ee6cdb9167
James Harrison <james@fasttortoise.co.uk>
2020-01-13 21:45:39 +0000

    Removed unused imports

--- src/main/java/net/capecraft/bungee/events/PlaytimeEventHandler.java
+++ src/main/java/net/capecraft/bungee/events/PlaytimeEventHandler.java
@@ -11,7 +11,6 @@ import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PostLoginEvent;
-import net.md_5.bungee.api.event.ServerDisconnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.config.Configuration;
 import net.md_5.bungee.event.EventHandler;
