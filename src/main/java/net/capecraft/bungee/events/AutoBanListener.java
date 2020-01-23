0b51a103e6bbbb3b51dc86f323cfc80ed4be4224
James Harrison <james@fasttortoise.co.uk>
2020-01-23 18:32:49 +0000

    Edited the listener

--- src/main/java/net/capecraft/bungee/events/AutoBanListener.java
+++ src/main/java/net/capecraft/bungee/events/AutoBanListener.java
@@ -1,12 +1,19 @@
 package net.capecraft.bungee.events;
 
 import me.leoko.advancedban.bungee.event.PunishmentEvent;
+import me.leoko.advancedban.utils.Punishment;
+import me.leoko.advancedban.utils.PunishmentType;
 import net.md_5.bungee.api.plugin.Listener;
 
 public class AutoBanListener implements Listener {
 
-	public void onPunishment(PunishmentEvent event) {
-		
+	
+	
+	public static void onPunishment(PunishmentEvent event) {
+		Punishment punish = event.getPunishment();
+		if(punish.getType() == PunishmentType.BAN || punish.getType() == PunishmentType.TEMP_BAN) {
+			punish.getUuid();
+		}
 	}
 
 }
